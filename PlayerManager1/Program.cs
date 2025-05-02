d788ef76bc8cfd57285b3283a7d41b262e21f7ab
ivyycore <mariasequeira.dev@gmail.com>
2025-05-02 13:16:18 +0100

    Created Name class

---
+++ PlayerManager1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PlayerManager1
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP1!");
+        }
+    }
+}
